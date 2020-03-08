165937be056ce7501ba9a650976aade64874c413
GabrielMendonca <gabba.kumbuca@gmail.com>
2020-03-08 13:34:50 -0300

    Add menus server_testes e ssh_alcatel

--- view.py
+++ view.py
@@ -1,9 +1,43 @@
+from getpass import getpass
+
 def imprimir(imprimir):
+    print(imprimir)
+
+def ter_imprimir(imprimir):
     if imprimir:
         for frases in imprimir[2:-1].split('\\r\\n'):
             print(frases)
     else:
         print("Comando retornou vazio !!!")
 
-def entrada():
+def entrada(texto=None):
+    if texto:
+        print("\n", texto, "\n")
     return input("Escreva <<< ")
+
+def entrada_senha(texto=None):
+    if texto:
+        print("\n", texto, "\n")
+    return getpass(prompt="Escreva <<< ")
+
+
+# ----- MENUS -----
+
+def menu_server_testes():
+    return entrada(
+        """
+        Escolha :
+        (0) >>> Assimir o terminal 
+        (1) >>> ssh 
+        (2) >>> telnet
+        (3) >>> ping
+        """
+    )
+
+def menu_ssh_alcatel():
+    return entrada(
+        """
+        Escolha :
+        (1) >>> Acessar o router 
+        """
+    )
